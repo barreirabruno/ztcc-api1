aa41c7e1dd7e5c13b855a12e6cbeb51036fe0655
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-13 19:34:17 -0300

    feat: use createTransactionAccount dependency

--- src/data/services/transaction-account-service.ts
+++ src/data/services/transaction-account-service.ts
@@ -1,14 +1,21 @@
 import { TransactionAccount } from '@/domain/features'
-import { NotFoundError } from '@/domain/models/errors'
-import { LoadTransactionAccout } from '../contracts/apis'
+import { TransactionAccountModel } from '@/domain/models'
+import { InternalServerError } from '@/domain/models/errors'
+import { CreateTransactionAccoutRepository, LoadTransactionAccoutRepository } from '../contracts/repos'
 
 export class TransactionAccountService {
   constructor (
-    private readonly loadTA: LoadTransactionAccout
+    private readonly loadTransactionAccount: LoadTransactionAccoutRepository,
+    private readonly createTransactionAccount: CreateTransactionAccoutRepository
   ) {}
 
-  async perform (input: TransactionAccount.Input): Promise<NotFoundError> {
-    await this.loadTA.loadTransactionAccount(input)
-    return new NotFoundError()
+  async perform (input: TransactionAccount.Input): Promise<null | InternalServerError |TransactionAccountModel> {
+    const searchTransactionAccount = await this.loadTransactionAccount.load({ vatNumber: input.vatNumber })
+    if (searchTransactionAccount !== null) {
+      return searchTransactionAccount
+    } else {
+      const createTransactionAccount = await this.createTransactionAccount.create(input)
+      return createTransactionAccount
+    }
   }
 }
