ba26fdbe83c5203f69226c627979d41c774468de
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-13 17:28:59 -0300

    refactor: change sut creation strategy to use beforeEach

--- tests/data/services/transaction-account.spec.ts
+++ tests/data/services/transaction-account.spec.ts
@@ -4,27 +4,20 @@ import { NotFoundError } from '@/domain/models/errors'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
-type SutTypes = {
-  sut: TransactionAccountService
-  loadTAByVatNumber: MockProxy<LoadTransactionAccout>
-}
-
-const makeSut = (): SutTypes => {
-  const loadTAByVatNumber = mock<LoadTransactionAccout>()
-  const sut = new TransactionAccountService(loadTAByVatNumber)
-  return {
-    sut,
-    loadTAByVatNumber
-  }
-}
-
-const fakeParams = {
-  vatNumber: 'any_vatNumber'
-}
-
 describe('TransactionAccountService', () => {
+  let loadTAByVatNumber: MockProxy<LoadTransactionAccout>
+  let sut: TransactionAccountService
+
+  const fakeParams = {
+    vatNumber: 'any_vatNumber'
+  }
+
+  beforeEach(() => {
+    loadTAByVatNumber = mock()
+    sut = new TransactionAccountService(loadTAByVatNumber)
+  })
+
   it('should call LoadTransactionAccout with correct params', async () => {
-    const { sut, loadTAByVatNumber } = makeSut()
     await sut.perform(fakeParams)
 
     expect(loadTAByVatNumber.loadTransactionAccount).toHaveBeenCalledWith(fakeParams)
@@ -32,7 +25,6 @@ describe('TransactionAccountService', () => {
   })
 
   it('should return NotFoundError if LoadTransactionAccout returns null', async () => {
-    const { sut, loadTAByVatNumber } = makeSut()
     loadTAByVatNumber.loadTransactionAccount.mockResolvedValueOnce(null)
 
     const loadTransactionAccount = await sut.perform(fakeParams)
