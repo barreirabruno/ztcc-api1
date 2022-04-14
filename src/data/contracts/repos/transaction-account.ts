10cacca9dc7e133ffde0f6b04b76f673cdb37d69
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-14 14:10:07 -0300

    feat: add UpdateTransactionAccoutRepository and fix input and output problemas

--- src/data/contracts/repos/transaction-account.ts
+++ src/data/contracts/repos/transaction-account.ts
@@ -17,7 +17,7 @@ export namespace LoadTransactionAccout {
 }
 
 export interface CreateTransactionAccoutRepository {
-  create: (input: LoadTransactionAccout.Input) => Promise<LoadTransactionAccout.Output>
+  create: (input: CreateTransactionAccoutRepository.Input) => Promise<CreateTransactionAccoutRepository.Output>
 }
 
 export namespace CreateTransactionAccoutRepository {
@@ -25,6 +25,23 @@ export namespace CreateTransactionAccoutRepository {
     first_name: string
     last_name: string
     vatNumber: string
+  }
+  export type Output = null | InternalServerError | {
+    id: string
+    first_name: string
+    last_name: string
+    vatNumber: string
+  }
+}
+
+export interface UpdateTransactionAccoutRepository {
+  update: (input: UpdateTransactionAccoutRepository.Input) => Promise<UpdateTransactionAccoutRepository.Output>
+}
+
+export namespace UpdateTransactionAccoutRepository {
+  export type Input = {
+    first_name?: string
+    last_name?: string
   }
   export type Output = null | {
     id: string
