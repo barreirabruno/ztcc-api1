6021efa231f7e7f1bcd3929cd9d95775bdb29c59
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-14 23:57:45 -0300

    refactor: change update/create repo method to save method

--- src/data/contracts/repos/transaction-account.ts
+++ src/data/contracts/repos/transaction-account.ts
@@ -16,34 +16,18 @@ export namespace LoadTransactionAccout {
   }
 }
 
-export interface CreateTransactionAccoutRepository {
-  create: (input: CreateTransactionAccoutRepository.Input) => Promise<CreateTransactionAccoutRepository.Output>
+export interface SaveTransactionAccoutRepository {
+  save: (input: SaveTransactionAccoutRepository.Input) => Promise<SaveTransactionAccoutRepository.Output>
 }
 
-export namespace CreateTransactionAccoutRepository {
+export namespace SaveTransactionAccoutRepository {
   export type Input = {
+    id?: string
     first_name: string
     last_name: string
-    vatNumber: string
+    vatNumber?: string
   }
   export type Output = null | InternalServerError | {
-    id: string
-    first_name: string
-    last_name: string
-    vatNumber: string
-  }
-}
-
-export interface UpdateTransactionAccoutRepository {
-  update: (input: UpdateTransactionAccoutRepository.Input) => Promise<UpdateTransactionAccoutRepository.Output>
-}
-
-export namespace UpdateTransactionAccoutRepository {
-  export type Input = {
-    first_name?: string
-    last_name?: string
-  }
-  export type Output = null | {
     id: string
     first_name: string
     last_name: string
