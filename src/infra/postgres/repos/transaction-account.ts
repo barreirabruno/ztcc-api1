5c675914a5bc3d0128f277d9a8d1395cb80440a8
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-15 19:49:16 -0300

    refactor: move PgTransactionAccountRepository to a single file

---
+++ src/infra/postgres/repos/transaction-account.ts
@@ -0,0 +1,18 @@
+import { LoadTransactionAccout } from '@/data/contracts/repos'
+import { getRepository } from 'typeorm'
+import { PgTransactionAccount } from '../entities'
+
+export class PgTransactionAccountRepository {
+  async load (params: LoadTransactionAccout.Input): Promise<LoadTransactionAccout.Output> {
+    const pgTransactionAccountRepo = getRepository(PgTransactionAccount)
+    const pgTA = await pgTransactionAccountRepo.findOne({ vatNumber: params.vatNumber })
+    if (pgTA !== undefined) {
+      return {
+        id: pgTA.id.toString(),
+        first_name: pgTA.first_name ?? undefined,
+        last_name: pgTA.last_name ?? undefined,
+        vatNumber: pgTA.vatNumber
+      }
+    }
+  }
+}
