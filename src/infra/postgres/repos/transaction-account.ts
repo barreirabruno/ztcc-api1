7a1ecc9c7661aae3e98114d26f6c7e446b75c281
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-15 20:50:01 -0300

    refactor: make pgTransactionAccountRepo private property on PgTransactionAccountRepository

--- src/infra/postgres/repos/transaction-account.ts
+++ src/infra/postgres/repos/transaction-account.ts
@@ -3,9 +3,10 @@ import { getRepository } from 'typeorm'
 import { PgTransactionAccount } from '../entities'
 
 export class PgTransactionAccountRepository implements LoadTransactionAccoutRepository {
+  private readonly pgTransactionAccountRepo = getRepository(PgTransactionAccount)
+
   async load (params: LoadTransactionAccout.Input): Promise<LoadTransactionAccout.Output> {
-    const pgTransactionAccountRepo = getRepository(PgTransactionAccount)
-    const pgTA = await pgTransactionAccountRepo.findOne({ vatNumber: params.vatNumber })
+    const pgTA = await this.pgTransactionAccountRepo.findOne({ vatNumber: params.vatNumber })
     if (pgTA !== undefined) {
       return {
         id: pgTA.id.toString(),
@@ -17,15 +18,14 @@ export class PgTransactionAccountRepository implements LoadTransactionAccoutRepo
   }
 
   async save (params: SaveTransactionAccoutRepository.Input): Promise<void> {
-    const pgTransactionAccountRepo = getRepository(PgTransactionAccount)
     if (params.id === undefined) {
-      await pgTransactionAccountRepo.save({
+      await this.pgTransactionAccountRepo.save({
         first_name: params.first_name,
         last_name: params.last_name,
         vatNumber: params.vatNumber
       })
     } else {
-      await pgTransactionAccountRepo.update({
+      await this.pgTransactionAccountRepo.update({
         id: parseInt(params.id)
       }, {
         first_name: params.first_name,
