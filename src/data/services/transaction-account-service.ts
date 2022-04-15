a3c7eeb34ef29d17836e58996bc51e8e76bee50c
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-14 23:56:48 -0300

    refactor: remove implementation details from service

--- src/data/services/transaction-account-service.ts
+++ src/data/services/transaction-account-service.ts
@@ -1,26 +1,21 @@
 import { TransactionAccount } from '@/domain/features'
 import { InternalServerError } from '@/domain/models/errors'
-import { CreateTransactionAccoutRepository, LoadTransactionAccoutRepository, UpdateTransactionAccoutRepository } from '../contracts/repos'
+import { SaveTransactionAccoutRepository, LoadTransactionAccoutRepository } from '../contracts/repos'
 
 export class TransactionAccountService {
   constructor (
     private readonly userAccountRepo: LoadTransactionAccoutRepository &
-    CreateTransactionAccoutRepository &
-    UpdateTransactionAccoutRepository
+    SaveTransactionAccoutRepository
   ) {}
 
   async perform (input: TransactionAccount.Input): Promise<InternalServerError> {
     const searchTransactionAccount = await this.userAccountRepo.load({ vatNumber: input.vatNumber })
-    if (searchTransactionAccount !== null) {
-      if (searchTransactionAccount?.id !== undefined) {
-        await this.userAccountRepo.update({
-          first_name: input.first_name,
-          last_name: input.last_name
-        })
-      }
-    } else {
-      await this.userAccountRepo.create(input)
-    }
+    await this.userAccountRepo.save({
+      id: searchTransactionAccount?.id ?? input.id,
+      first_name: input.first_name,
+      last_name: input.last_name,
+      vatNumber: input.vatNumber
+    })
     return new InternalServerError()
   }
 }
