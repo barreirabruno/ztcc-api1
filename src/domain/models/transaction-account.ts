54e7abab6f04a0bc0d4eed19cfef2f0dde363466
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-15 13:51:47 -0300

    refactor: move TransactionAccountModel to a class

--- src/domain/models/transaction-account.ts
+++ src/domain/models/transaction-account.ts
@@ -1,6 +1,28 @@
-export type TransactionAccountModel = {
-  id: string
-  first_name: string
-  last_name: string
+type TransactionAccountData = {
+  id?: string
+  first_name?: string
+  last_name?: string
   vatNumber: string
+}
+
+type LoadTransactionAccountData = {
+  id: string
+  first_name?: string
+  last_name?: string
+  vatNumber: string
+} | null | undefined
+
+export class TransactionAccountModel {
+  id?: string
+  first_name?: string
+  last_name?: string
+  vatNumber: string
+
+  constructor (transactionAccountData: TransactionAccountData,
+    loadTransactionAccount?: LoadTransactionAccountData) {
+    this.id = loadTransactionAccount?.id ?? transactionAccountData.id
+    this.first_name = transactionAccountData.first_name ?? loadTransactionAccount?.first_name
+    this.last_name = transactionAccountData.last_name ?? loadTransactionAccount?.last_name
+    this.vatNumber = loadTransactionAccount?.vatNumber ?? transactionAccountData.vatNumber
+  }
 }
