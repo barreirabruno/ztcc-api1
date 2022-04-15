bda111dff927d524aaaa8806ffe77183b3773b7c
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-15 19:48:25 -0300

    refactor: move PgTransactionAccount to a single file

---
+++ src/infra/postgres/entities/transaction-account.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity()
+export class PgTransactionAccount {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column({ nullable: true })
+  first_name!: string
+
+  @Column({ nullable: true })
+  last_name!: string
+
+  @Column()
+  vatNumber!: string
+}
