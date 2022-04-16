0dbb1635eb41d5b434727f2d1efb06641af25270
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-16 12:33:53 -0300

    feat: add badRequest and serverError http helper

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -2,3 +2,13 @@ export type HttpResponse = {
   statusCode: number
   data: any
 }
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  data: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  data: error
+})
