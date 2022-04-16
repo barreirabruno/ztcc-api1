66319c5fc02b6deaefb72c9f145b0a70fd0bb497
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-15 23:35:11 -0300

    test: ensure FacebookLoginController returns 400 if vatNumber is empty

---
+++ src/application/controllerrs/transaction-account.spec.ts
@@ -0,0 +1,29 @@
+type HttpResponse = {
+  statusCode: number
+  data: any
+}
+
+class TransactionController {
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    return {
+      statusCode: 400,
+      data: new Error('The field vatNumber is required')
+    }
+  }
+}
+
+describe('TransactionAccountController', () => {
+  it('should return 400 if vatNumber is empty', async () => {
+    const sut = new TransactionController()
+
+    const httpResponse = await sut.handle({
+      first_name: 'any_user_name',
+      last_name: 'any_last_user_name'
+    })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field vatNumber is required')
+    })
+  })
+})
