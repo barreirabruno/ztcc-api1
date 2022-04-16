a65d00cf297f8c3accf572adb141e53aeb54403c
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-16 16:23:53 -0300

    refactor: add generics for http helpers

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,21 +1,21 @@
 import { ServerError } from '@/domain/models/errors'
 
-export type HttpResponse = {
+export type HttpResponse<T = any> = {
   statusCode: number
-  data: any
+  data: T
 }
 
-export const ok = (data: any): HttpResponse => ({
+export const ok = <T = any> (data: T): HttpResponse<T> => ({
   statusCode: 200,
   data: data
 })
 
-export const badRequest = (error: Error): HttpResponse => ({
+export const badRequest = (error: Error): HttpResponse<Error> => ({
   statusCode: 400,
   data: error
 })
 
-export const serverError = (error: Error): HttpResponse => ({
+export const serverError = (error: Error): HttpResponse<Error> => ({
   statusCode: 500,
   data: new ServerError(error)
 })
