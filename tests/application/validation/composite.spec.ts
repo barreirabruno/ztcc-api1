4d460f6b82a8349a788fc49e4885c743547af7ef
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-16 17:47:41 -0300

    test: ensure ValidationComposite returns the first error

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -8,7 +8,12 @@ class ValidationComposite {
   constructor (private readonly validators: Validator[]) {}
 
   validate (): Error | undefined {
-    return undefined
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error !== undefined) {
+        return error
+      }
+    }
   }
 }
 
@@ -34,5 +39,13 @@ describe('ValidationComposite', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
+  })
+
+  it('should return the first error', () => {
+    validator1.validate.mockReturnValue(new Error('error_1'))
+    validator2.validate.mockReturnValue(new Error('error_2'))
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
   })
 })
