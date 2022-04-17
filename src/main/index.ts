e174120d65f29c00e0f1b5da840562bafd701bb0
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-17 18:33:43 -0300

    refactorr: use ormconfig default file for database connection

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,5 +3,8 @@ import './config/module-alias'
 import 'reflect-metadata'
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
+import { createConnection } from 'typeorm'
 
-app.listen(env.serverPort, () => console.log(`[SERVER UP AND RUNNING AT PORT ${env.serverPort}]`))
+createConnection()
+  .then(() => app.listen(env.serverPort, () => console.log(`[SERVER UP AND RUNNING AT PORT ${env.serverPort}]`)))
+  .catch(console.error)
