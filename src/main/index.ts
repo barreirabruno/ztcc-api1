c654693c188a597e8edc5b1c881db3b1398bf518
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-17 23:10:31 -0300

    refactor: update index

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,7 @@ import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 import { createConnection } from 'typeorm'
 
-createConnection()
-  .then(() => app.listen(env.serverPort, () => console.log(`[SERVER UP AND RUNNING AT PORT ${env.serverPort}]`)))
+createConnection().then(() => {
+  app.listen(env.serverPort, () => console.log(`[SERVER UP AND RUNNING AT PORT ${env.serverPort}]`))
+})
   .catch(console.error)
