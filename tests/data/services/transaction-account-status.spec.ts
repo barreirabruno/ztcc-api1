d85b2d33723801f8d62612c5d1ef77c98b480098
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-21 17:37:27 -0300

    test: ensure it returns unavailable if active is not 1

--- tests/data/services/transaction-account-status.spec.ts
+++ tests/data/services/transaction-account-status.spec.ts
@@ -1,38 +1,77 @@
+import { LoadTransactionAccoutRepository } from '@/data/contracts/repos'
+import { mock, MockProxy } from 'jest-mock-extended'
+
 interface IsAccountActiveInterface {
   perform: (params: IsAccountActive.Input) => Promise<IsAccountActive.Output>
 }
 
 export namespace IsAccountActive {
+  export enum TransactionAccountStatusEnum {
+    AVAILABLE = 'available',
+    UNAVAILABLE = 'unavailable'
+  }
+
   export type Input = {
     vatNumber: string
   }
   export type Output = {
-    status: string
+    status: TransactionAccountStatusEnum
   }
 }
 
 class IsAccountActiveService implements IsAccountActiveInterface {
+  constructor (private readonly transactionAccountRepo: LoadTransactionAccoutRepository) {}
+
   async perform (params: IsAccountActive.Input): Promise<IsAccountActive.Output> {
-    return {
-      status: 'available'
+    const searchTransactionAccount = await this.transactionAccountRepo.load({ vatNumber: params.vatNumber })
+    if (searchTransactionAccount?.active === 1) {
+      return {
+        status: IsAccountActive.TransactionAccountStatusEnum.AVAILABLE
+      }
+    } else {
+      return {
+        status: IsAccountActive.TransactionAccountStatusEnum.UNAVAILABLE
+      }
     }
   }
 }
 
 describe('IsAccountActiveService', () => {
+  const mockTARepositoryData = {
+    id: 'any_database_id',
+    first_name: 'any_firstname',
+    last_name: 'any_lastname',
+    vatNumber: 'any_database_vatNumber'
+  }
+  let transactionAccountRepo: MockProxy<LoadTransactionAccoutRepository>
   let sut: IsAccountActiveService
 
   beforeAll(() => {
-    sut = new IsAccountActiveService()
+    transactionAccountRepo = mock()
+    sut = new IsAccountActiveService(transactionAccountRepo)
   })
 
   it('should return available if active is equal to 1', async () => {
+    transactionAccountRepo.load.mockResolvedValueOnce({
+      ...mockTARepositoryData,
+      active: 1
+    })
     const transactionAccountStatus = await sut.perform({ vatNumber: 'any_vatnumber' })
 
     expect(transactionAccountStatus).toEqual({
       status: 'available'
     })
   })
-  it('should return unavailable if active is not 1', () => {})
+  it('should return unavailable if active is not 1', async () => {
+    transactionAccountRepo.load.mockResolvedValueOnce({
+      ...mockTARepositoryData,
+      active: 0
+    })
+    const transactionAccountStatus = await sut.perform({ vatNumber: 'any_vatnumber' })
+
+    expect(transactionAccountStatus).toEqual({
+      status: 'unavailable'
+    })
+  })
   it('should return unavailable if transaction account is not found', () => {})
 })
