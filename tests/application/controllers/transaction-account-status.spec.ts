7a3c83d8a4ef29ebc37ce6de2f940786aa44999f
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-21 20:18:10 -0300

    test: ensure it returns 200 if perform method succeeds

---
+++ tests/application/controllers/transaction-account-status.spec.ts
@@ -0,0 +1,63 @@
+import { TransactionAccountStatusService } from '@/data/services'
+import { TransactionAccountStatus } from '@/domain/features'
+import { Controller } from '@/application/controllers'
+import { HttpResponse, ok, serverError } from '@/application/helpers'
+import { InternalServerError } from '@/domain/models/errors'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+type HttpRequest = {
+  vatNumber: string
+}
+
+type Model = Error | {
+  status: string
+}
+
+export class TransactionAccountStatusController extends Controller {
+  constructor (
+    private readonly transactionAccountStatusService: TransactionAccountStatusService
+  ) {
+    super()
+  }
+
+  async perform (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
+    const result = await this.transactionAccountStatusService.perform({
+      vatNumber: httpRequest.vatNumber
+    })
+    if (result.constructor === InternalServerError ||
+      result.constructor === Error) {
+      return serverError(result)
+    } else {
+      return ok(result)
+    }
+  }
+}
+
+describe('TransactionAccountStatus', () => {
+  let transactionAccountStatusService: MockProxy<TransactionAccountStatusService>
+  let sut: TransactionAccountStatusController
+
+  beforeAll(() => {
+    transactionAccountStatusService = mock()
+    transactionAccountStatusService.perform.mockResolvedValue({
+      status: TransactionAccountStatus.TransactionAccountStatusEnum.AVAILABLE
+    })
+    sut = new TransactionAccountStatusController(transactionAccountStatusService)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should build validators correctly', () => {})
+  it('should call TransactionAccountStatusService with correct params', () => {})
+  it('should return 200 if perform method succeeds', async () => {
+    const transactionAccountStatusService = await sut.handle({})
+    expect(transactionAccountStatusService).toEqual({
+      statusCode: 200,
+      data: {
+        status: TransactionAccountStatus.TransactionAccountStatusEnum.AVAILABLE
+      }
+    })
+  })
+})
