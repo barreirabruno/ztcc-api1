13972bf6863148b74a16ab62b5f15591ee5a18d1
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-21 23:01:39 -0300

    test: ensure it returns return empty if string is not too short

--- tests/application/validation/min-length-string.spec.ts
+++ tests/application/validation/min-length-string.spec.ts
@@ -1,35 +1,20 @@
-class TooShortStringError extends Error {
-  constructor (fieldName: string) {
-    super(`The field ${fieldName} is too short`)
-  }
-}
-
-class MinLengthStringValidator {
-  constructor (
-    private readonly minLengthValue: number,
-    private readonly value: string,
-    private readonly fieldName: string) {}
-
-  validate (): Error | undefined {
-    const rule = '/[0-9]{3,}/'
-    const minLengthRegex = new RegExp(rule)
-    const executeRegex = this.value.match(minLengthRegex)
-    if (executeRegex == null) {
-      return new TooShortStringError(this.fieldName)
-    }
-    return undefined
-  }
-}
+import { TooShortStringError } from '@/application/errors'
+import { MinLengthStringValidator } from '@/application/validation'
 
 describe('MinLengthStringValidator', () => {
   it('should return TooShortStringError if string has 2 characters', () => {
-    const sut = new MinLengthStringValidator(3, '00', 'vatNumber')
+    const sut = new MinLengthStringValidator('00', 'vatNumber')
 
     const error = sut.validate()
 
     expect(error).toEqual(new TooShortStringError('vatNumber'))
   })
-  it('should return empty if string is not too short', () => {
 
+  it('should return empty if string is not too short', () => {
+    const sut = new MinLengthStringValidator('00000000000', 'vatNumber')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(undefined)
   })
 })
