420e45228f8a818812eb2a49d96986e78cdd1659
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-22 01:15:56 -0300

    chore: ignore coverage on migration files

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,8 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/index.ts'],
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/infra/postgres/migrations/**.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   moduleNameMapper: {
